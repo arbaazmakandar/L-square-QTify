701e4f1e6890b104abed1c4c51f92697bc1230b8
arbaazmakandar <arbaazmakandar@gmail.com>
2024-05-18 22:33:01 +0530

    Added Hero Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 // import {Outlet} from 'react-router-dom'
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <Navbar/>
+      <Hero/>
     </StyledEngineProvider>
   );
 }
---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Headphones from '../../assets/headphones.png'
+import { Box,Typography } from '@mui/material'
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <Box className={styles.outerBox}>
+        <Typography variant="h4" className={styles.text}>
+        100 Thousand Songs, ad-free 
+        <br/>
+        Over thousands podcast episodes
+        </Typography>
+        <Box className={styles.image}><img src={Headphones} alt="Headphones" /></Box> 
+    </Box>
+  )
+}
+
+export default Hero
